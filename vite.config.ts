9291a45f4e1ba33f6a46bff355cff305e7df653a
Artem Gromakov <pandawithmelon@gmail.com>
2024-02-20 14:49:49 +0300

    Add environment plugin to resolve env in test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin("all")],
   base: "/frontend-challenge/",
 });
