6eec496f2c6d94bbe8f6bd5ec04af1592edf8923
Artem Gromakov <pandawithmelon@gmail.com>
2024-02-20 14:51:54 +0300

    import.meta =>>> process.env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,6 +4,6 @@ export const catnipThrower = axios.create({
   baseURL: " https://api.thecatapi.com/v1/images/search",
   params: {
     limit: 15,
-    api_key: `${import.meta.env.VITE_API_KEY}`,
+    api_key: `${process.env.VITE_API_KEY}`,
   },
 });
